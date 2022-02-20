c4df19fe9ea9564a9c40dc6a2fd84e774285992f
Jacob Cohn <jacobnc916@gmail.com>
2022-02-20 15:35:09 -0600

    added western conference nba teams

--- src/components/utils/nbaImages.js
+++ src/components/utils/nbaImages.js
@@ -4,14 +4,29 @@ import brooklynNetsLogo from '../../assets/brooklyn-nets-logo@logotyp.us.svg';
 import charlotteHornetsLogo from '../../assets/charlotte-hornets-logo@logotyp.us.svg';
 import chicagoBullsLogo from '../../assets/chicago-bulls-logo@logotyp.us.svg';
 import clevelandCavaliersLogo from '../../assets/cleveland-cavaliers-logo@logotyp.us.svg';
+import dallasMavericksLogo from '../../assets/dallas-mavericks-logo@logotyp.us.svg';
+import denverNuggetsLogo from '../../assets/denver-nuggets-logo@logotyp.us.svg';
 import detroitPistonsLogo from '../../assets/detroit-pistons-logo@logotyp.us.svg';
+import goldenStateWarriorsLogo from '../../assets/golden-state-warriors-logo.svg';
+import houstonRocketsLogo from '../../assets/houston-rockets-logo@logotyp.us.svg';
 import indianaPacersLogo from '../../assets/indiana-pacers-logo@logotyp.us.svg';
+import losAngelesClippersLogo from '../../assets/la-clippers-logo@logotyp.us.svg';
+import losAngelesLakersLogo from '../../assets/los-angeles-lakers-logo@logotyp.us.svg';
+import memphisGrizzliesLogo from '../../assets/memphis-grizzlies-logo.svg';
 import miamiHeatLogo from '../../assets/miami-heat-logo@logotyp.us.svg';
 import milwaukeeBucksLogo from '../../assets/milwaukee-bucks-logo@logotyp.us.svg';
+import minnesotaTimberwolvesLogo from '../../assets/timberwolves-logo@logotyp.us.svg';
+import newOrleansPelicansLogo from '../../assets/pelicans-logo@logotyp.us.svg';
 import newYorkKnicksLogo from '../../assets/new-york-knicks-logo@logotyp.us.svg';
+import oklahomaCityThunderLogo from '../../assets/okc-thunder-logo@logotyp.us.svg';
 import orlandoMagicLogo from '../../assets/orlando-magic-logo@logotyp.us.svg';
 import philadelphia76ersLogo from '../../assets/philadelphia-76ers-logo@logotyp.us.svg';
+import phoenixSunsLogo from '../../assets/phoenix-suns-logo@logotyp.us.svg';
+import portlandTrailBlazersLogo from '../../assets/portland-trail-blazers-logo@logotyp.us.svg';
+import sacramentoKingsLogo from '../../assets/sacramento-kings-logo@logotyp.us.svg';
+import sanAntonioSpursLogo from '../../assets/spurs-logo@logotyp.us.svg';
 import torontoRaptorsLogo from '../../assets/toronto-raptors-logo@logotyp.us.svg';
+import utahJazzLogo from '../../assets/utah-jazz-logo@logotyp.us.svg';
 import washingtonWizardsLogo from '../../assets/washington-wizards-logo@logotyp.us.svg';
 
 const nbaImages = [
@@ -19,77 +34,152 @@ const nbaImages = [
     id: "0", 
     name: "Atlanta Hawks", 
     src: atlantaHawksLogo, 
-  }, 
+  },
   {
     id: "1", 
     name: "Boston Celtics", 
     src: bostonCelticsLogo, 
-  }, 
+  },
   {
     id: "2", 
     name: "Brooklyn Nets", 
     src: brooklynNetsLogo, 
-  }, 
+  },
   {
     id: "3", 
     name: "Charlotte Hornets", 
     src: charlotteHornetsLogo, 
-  }, 
+  },
   {
     id: "4", 
     name: "Chicago Bulls", 
     src: chicagoBullsLogo, 
-  }, 
+  },
   {
     id: "5", 
     name: "Cleveland Cavaliers", 
     src: clevelandCavaliersLogo, 
-  }, 
+  },
   {
     id: "6", 
-    name: "Detroit Pistons", 
-    src: detroitPistonsLogo, 
-  }, 
+    name: "Dallas Mavericks", 
+    src: dallasMavericksLogo, 
+  },
   {
     id: "7", 
-    name: "Indiana Pacers", 
-    src: indianaPacersLogo, 
-  }, 
+    name: "Denver Nuggets", 
+    src: denverNuggetsLogo, 
+  },
   {
     id: "8", 
-    name: "Miami Heat", 
-    src: miamiHeatLogo, 
-  }, 
+    name: "Detroit Pistons", 
+    src: detroitPistonsLogo, 
+  },
   {
     id: "9", 
-    name: "Milwaukee Bucks", 
-    src: milwaukeeBucksLogo, 
-  }, 
+    name: "Golden State Warriors", 
+    src: goldenStateWarriorsLogo, 
+  },
   {
     id: "10", 
-    name: "New York Knicks", 
-    src: newYorkKnicksLogo, 
-  }, 
+    name: "Houston Rockets", 
+    src: houstonRocketsLogo, 
+  },
   {
     id: "11", 
-    name: "Orlando Magic", 
-    src: orlandoMagicLogo, 
-  }, 
+    name: "Indiana Pacers", 
+    src: indianaPacersLogo, 
+  },
   {
     id: "12", 
-    name: "Philadelphia 76ers", 
-    src: philadelphia76ersLogo, 
-  }, 
+    name: "Los Angeles Clippers", 
+    src: losAngelesClippersLogo, 
+  },
   {
     id: "13", 
-    name: "Toronto Raptors", 
-    src: torontoRaptorsLogo, 
-  }, 
+    name: "Los Angeles Lakers", 
+    src: losAngelesLakersLogo, 
+  },
   {
     id: "14", 
+    name: "Memphis Grizzlies", 
+    src: memphisGrizzliesLogo, 
+  },
+  {
+    id: "15", 
+    name: "Miami Heat", 
+    src: miamiHeatLogo, 
+  },
+  {
+    id: "16", 
+    name: "Milwaukee Bucks", 
+    src: milwaukeeBucksLogo, 
+  },
+  {
+    id: "17", 
+    name: "Minnesota Timberwolves", 
+    src: minnesotaTimberwolvesLogo, 
+  },
+  {
+    id: "18", 
+    name: "New Orleans Pelicans", 
+    src: newOrleansPelicansLogo, 
+  },
+  {
+    id: "19", 
+    name: "New York Knicks", 
+    src: newYorkKnicksLogo, 
+  },
+  {
+    id: "20", 
+    name: "Oklahoma City Thunder", 
+    src: oklahomaCityThunderLogo, 
+  },
+  {
+    id: "21", 
+    name: "Orlando Magic", 
+    src: orlandoMagicLogo, 
+  },
+  {
+    id: "22", 
+    name: "Philadelphia 76ers", 
+    src: philadelphia76ersLogo, 
+  },
+  {
+    id: "23", 
+    name: "Phoenix Suns", 
+    src: phoenixSunsLogo, 
+  },
+  {
+    id: "24", 
+    name: "Portland Trail Blazers", 
+    src: portlandTrailBlazersLogo, 
+  },
+  {
+    id: "25", 
+    name: "Sacramento Kings", 
+    src: sacramentoKingsLogo, 
+  },
+  {
+    id: "26", 
+    name: "San Antonio Spurs", 
+    src: sanAntonioSpursLogo, 
+  },
+  {
+    id: "27", 
+    name: "Toronto Raptors", 
+    src: torontoRaptorsLogo, 
+  },
+  {
+    id: "28", 
+    name: "Utah Jazz", 
+    src: utahJazzLogo, 
+  },
+  {
+    id: "29", 
     name: "Washington Wizards", 
     src: washingtonWizardsLogo, 
-  }, 
+  },
 ];
 
 export default nbaImages;
